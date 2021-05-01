c2d9c0327ea189857686e4e31397537ed24da262
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2021-05-01 13:28:30 +0530

    Gir

---
+++ wzxt/dashboard/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BackroundConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'backround'
---
+++ wzxt/runserver.cmd
@@ -0,0 +1,4 @@
+cd..
+\create\Scripts\activate.bat
+cd wzxt
+python manage.py runserver 8080
