96dc0df1e0a0b1eae9fdc8a048ff07c692527857
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2021-05-01 16:03:44 +0530

    t

--- wzxt_django/dashboard/apps.py
+++ wzxt_django/dashboard/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BackroundConfig(AppConfig):
+class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backround'
+    name = 'dashboard'
